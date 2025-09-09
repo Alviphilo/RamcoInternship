6293aa766c7ebc0c8c485709818c7a55cd9d7ed6
Alvina Philomin <149966614+Alviphilo@users.noreply.github.com>
2025-09-09 09:59:13 +0530

    Delete Dashboard/highlights.js

--- Dashboard/highlights.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const rows = document.querySelectorAll("#serverTable tbody tr");
-    rows.forEach(row => {
-        row.addEventListener("click", () => {
-            rows.forEach(r => r.classList.remove("active"));
-            row.classList.add("active");
-        });
-    });
-});
